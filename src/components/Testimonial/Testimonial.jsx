965bdae13b432edfc60c8b9a7a4b074dd73a7186
Jasharaj <jasharaj38624@outlook.com>
2025-01-03 12:48:51 +0530

    N/A

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -60,7 +60,7 @@ const Testimonial = () => {
                             <img src={patientAvatar} alt="" />
                             <div>
                                 <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
-                                    Muhibur Rahman
+                                    Jasharaj
                                 </h4>
                                 <div className='flex items-center gap-[2px]'>
                                     <HiStar className='text-yellowColor w-[18px] h-5' />
@@ -85,7 +85,7 @@ const Testimonial = () => {
                             <img src={patientAvatar} alt="" />
                             <div>
                                 <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
-                                    Muhibur Rahman
+                                    Jasharaj
                                 </h4>
                                 <div className='flex items-center gap-[2px]'>
                                     <HiStar className='text-yellowColor w-[18px] h-5' />
@@ -110,7 +110,7 @@ const Testimonial = () => {
                             <img src={patientAvatar} alt="" />
                             <div>
                                 <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
-                                    Muhibur Rahman
+                                    Jasharaj
                                 </h4>
                                 <div className='flex items-center gap-[2px]'>
                                     <HiStar className='text-yellowColor w-[18px] h-5' />
